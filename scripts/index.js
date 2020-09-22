c4691954ccebd23539c3e94dadcad80f678e678c
Simon97i <softimag08@gmail.com>
2020-09-22 11:56:07 +0200

    ft(article): connect edit article page

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const blogCard = document.querySelectorAll('.card');
+
+blogCard.forEach(card => card.addEventListener('click', openPost))
+
+function openPost() {
+    location.href = './blog/article.html'
+}
